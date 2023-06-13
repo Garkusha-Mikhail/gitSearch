80f209f9c9a5c51742089e75ea6ecad983dfb30b
Misha <mask-wotz@yandex.ru>
2023-06-13 18:23:02 +0300

    получаю ответ от сервера. вывожу названия репозиториев в количестве пяти штук под блоком ввода. надо прикрутить очистку при продолжении ввода.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+/* 
 
 class View {
     constructor(){
@@ -35,9 +35,17 @@ class View {
     
     createBlock(blockData) {
         const blockElement = this.createElement('li', 'repository');
-        blockElement.innerHTML = `<img class="user-prev-photo" src="${blockData.avatar_url}" alt="${blockData.login}"> <span class="user-prev-name">${blockData.login}</span>`;
+        blockElement.innerHTML = `
+        <p>${blockData.name}</p>
+        `;
 
         this.blockList.append(blockElement);
+    }
+
+    clearBlock(contain) {
+        contain.forEach(element => {
+            element.remove();
+        });
     }
 };
 
@@ -49,17 +57,80 @@ class Search {
     }
 
     async searchData() {
-        return await fetch(`https://api.github.com/search/users?q=${this.view.searchInput.value}`)
+        return await fetch(`https://api.github.com/search/repositories?q=${this.view.searchInput.value}`) 
         .then((res) => {
+            const blockList = document.querySelector('.blocks');
+            const elements = blockList.querySelectorAll('.repository');
+            if (elements.length > 0) {
+                this.view.clearBlock(elements);
+            }
           if(res.ok) {
             res.json().then(res => {
-                
-              res.items.forEach(data => this.view.createBlock(data))
+                for (let i=0; i<2; i++) {
+                    this.view.createBlock(res.items[i])
+                    
+                }
             });
           }
         } )
     }
 
 };
+new Search(new View()); */
 
-new Search(new View());
+/* async function showRes() {
+    const test = await fetch("https://api.github.com/search/repositories?q=react").then(res => res.json().then(res => res.items));
+    for (let i = 0; i<5; i++) {
+        console.log(test[i]);
+        const block = document.createElement('div');
+        block.insertAdjacentHTML("afterbegin", `<p class='p'>${test[i].name}</p> <p class='clear'>${test[i].owner.login}.login</p> <p>${test[i].stargazers_count}</p>`);
+        const p = block.querySelector('.p');
+        p.addEventListener('click', function () {
+            console.log('click on name');
+        })
+        const clear = block.querySelector('.clear');
+        clear.addEventListener('click', function (event) {
+            event.stopPropagation();
+            console.log('click on owner');           
+        });
+    const app = document.querySelector('.app');
+    app.append(block);
+    }}
+document.addEventListener('click', showRes); */
+
+
+const resultField = document.querySelector('.result-field');
+const inputField = document.querySelector('.input-field');
+
+function createElement(elementTag, elementClass) {
+    const element = document.createElement(elementTag);
+        if (elementClass) {
+            element.classList.add(elementClass);
+        }
+        return element;
+}
+
+async function search() {
+    const inputData = inputField.querySelector('input');
+        const fetchData = await fetch(`https://api.github.com/search/repositories?q=${inputData.value}`).then(res => res.json().then(res => res.items));
+    for (let i=0; i<5; i++){        
+        const variant = createElement('div', 'variant-block');
+        variant.insertAdjacentHTML("afterbegin", `<p class='p'>${fetchData[i].name}</p>`);
+        inputField.append(variant);
+    }
+}
+
+function debounce(fn, timeout) {
+    let timer = null;
+    return (...args) => {
+	clearTimeout(timer);
+	return new Promise((resolve) => {
+        timer = setTimeout(
+		() => resolve(fn(...args)),
+		timeout,);
+	});
+    };
+}
+
+const debouncedInput = debounce(search, 700)
+inputField.addEventListener('keyup', debouncedInput);
